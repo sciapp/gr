ffd6a4ec666b3253597338b6fddb248f73188031
Christian Felder <c.felder@fz-juelich.de>
2013-11-13 15:36:43 +0100

    pygr: new base class GRDrawAttributes, new package gr.pygr.helper
    GRDrawAttributes: new base class for pygr classes with line and marker properties
    gr.pygr.helper: refactor: renamed Helper to DomainChecker and moved this and
    ColorIndexGenerator to new package
    ErrorBar: now derived from base class GRDrawAttributes, use line and marker
    attributes in drawGR method
    PlotCurve: now derived from base class GRDrawAttributes, replaces
    getters and setters for LineType, MarkerType, LineColor, MarkerColor

--- lib/gr/python/gr/pygr/base.py
+++ lib/gr/python/gr/pygr/base.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 """..."""
 
-__author__  = "Christian Felder <c.felder@fz-juelich.de>"
-__date__    = "2013-08-22"
+# local library
+import gr
+from gr.pygr.helper import ColorIndexGenerator
+
+__author__ = "Christian Felder <c.felder@fz-juelich.de>"
+__date__ = "2013-11-13"
 __version__ = "0.3.0"
 __copyright__ = """Copyright 2012, 2013 Forschungszentrum Juelich GmbH
 
@@ -30,6 +34,53 @@ along with GR. If not, see <http://www.gnu.org/licenses/>.
 """
 
 class GRMeta(object):
-    
+
     def drawGR(self):
         raise NotImplementedError
+
+class GRDrawAttributes(object):
+
+    def __init__(self, linetype=gr.LINETYPE_SOLID, markertype=gr.MARKERTYPE_DOT,
+                 linecolor=None, markercolor=1):
+        self._linetype, self._markertype = linetype, markertype
+        self._linecolor, self._markercolor = linecolor, markercolor
+        if linecolor is None:
+            self._linecolor = ColorIndexGenerator.nextColorIndex()
+        else:
+            self._linecolor = linecolor
+
+    @property
+    def linetype(self):
+        """Get current linetype."""
+        return self._linetype
+
+    @linetype.setter
+    def linetype(self, value):
+        self._linetype = value
+
+    @property
+    def markertype(self):
+        """Get current markertype."""
+        return self._markertype
+
+    @markertype.setter
+    def markertype(self, value):
+        self._markertype = value
+
+    @property
+    def linecolor(self):
+        """Get current linecolor."""
+        return self._linecolor
+
+    @linecolor.setter
+    def linecolor(self, value):
+        self._linecolor = value
+
+    @property
+    def markercolor(self):
+        """Get current markercolor."""
+        return self._markercolor
+
+    @markercolor.setter
+    def markercolor(self, value):
+        self._markercolor = value
