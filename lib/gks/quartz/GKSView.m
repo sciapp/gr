93f2bb5a06e4bb8cd0c501d1df7f2e7f160f9dfc
Josef Heinen <j.heinen@fz-juelich.de>
2014-01-07 21:56:43 +0100

    Fixed 'cellarray/drawimage' performance problem

--- lib/gks/quartz/GKSView.m
+++ lib/gks/quartz/GKSView.m
@@ -373,7 +373,7 @@ void seg_xform_rel(double *x, double *y)
           memcpy(gkss, sl, sizeof(gks_state_list_t));
 
           p->width  = [self bounds].size.width;
-          p->height = [self bounds].size.height;
+          p->height = [self bounds].size.height - TITLE_BAR_HEIGHT;
 
           p->swidth  = NSMaxX([[[NSScreen screens] objectAtIndex:0] frame]);
           p->sheight = NSMaxY([[[NSScreen screens] objectAtIndex:0] frame]);
