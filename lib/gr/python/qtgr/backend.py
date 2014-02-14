4e4834e984d0589c95e486f1fbf3445a2e1a0da2
Christian Felder <c.felder@fz-juelich.de>
2014-02-14 13:50:02 +0100

    qtgr: choose python qt backend
    added missing qtgr.backend package

---
+++ lib/gr/python/qtgr/backend.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Qt backend GR module
+
+The default backend order (PySide, PyQt4) can be overwritten with:
+    setattr(sys, "QT_BACKEND_ORDER", ["PyQt4", "PySide"])
+"""
+
+import sys
+
+__author__ = "Christian Felder <c.felder@fz-juelich.de>"
+__date__ = "2014-02-14"
+__version__ = "0.4.0"
+__copyright__ = """Copyright 2012-2014 Forschungszentrum Juelich GmbH
+
+This file is part of GR, a universal framework for visualization applications.
+Visit https://iffwww.iff.kfa-juelich.de/portal/doku.php?id=gr for the latest
+version.
+
+GR was developed by the Scientific IT-Systems group at the Peter Grünberg
+Institute at Forschunsgzentrum Jülich. The main development has been done
+by Josef Heinen who currently maintains the software.
+
+GR is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This framework is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GR. If not, see <http://www.gnu.org/licenses/>.
+ 
+"""
+
+QT_PYSIDE = "PySide"
+QT_PYQT4 = "PyQt4"
+QT_BACKEND_ORDER = getattr(sys, "QT_BACKEND_ORDER", [QT_PYSIDE, QT_PYQT4])
+
+del sys
