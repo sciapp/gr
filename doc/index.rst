d2accf2882fe03f16c3c7d9ef898bfc3badc0194
Josef Heinen <j.heinen@fz-juelich.de>
2014-02-21 15:27:10 +0100

    Sphinx 1.2.1: update Makefiles and master index

--- doc/index.rst
+++ doc/index.rst
@@ -1,5 +1,5 @@
 .. gr documentation master file, created by
-   sphinx-quickstart on Tue Feb 11 08:54:16 2014.
+   sphinx-quickstart on Fri Feb 21 15:25:51 2014.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
--- doc/make.bat
+++ doc/make.bat
@@ -115,9 +115,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\gr-DocumentationandExamples.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\GRframework.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\gr-DocumentationandExamples.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\GRframework.ghc
 	goto end
 )
 
