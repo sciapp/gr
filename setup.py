9bd2c118e4b4fb4183549380c8145076819fb071
Josef Heinen <j.heinen@fz-juelich.de>
2014-07-18 11:12:18 +0200

    release v0.5.2
    Change-Id: I92cd8a0680560ba1bbada4564a80fdcf8269d4d3
    Reviewed-on: https://iffwww.iff.kfa-juelich.de/review/18
    Reviewed-by: Josef Heinen <j.heinen@fz-juelich.de>

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ from subprocess import Popen, PIPE, STDOUT
 
 
 __author__ = "Christian Felder <c.felder@fz-juelich.de>"
-__date__ = "2014-05-21"
-__version__ = "0.5.1"
+__date__ = "2014-07-18"
+__version__ = "0.5.2"
 __copyright__ = """Copyright 2012 - 2014 Forschungszentrum Juelich GmbH
 
 This file is part of GR, a universal framework for visualization applications.
