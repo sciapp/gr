c7e64bc050c2f8385078be25c07b957b64e9bf73
Josef Heinen <j.heinen@fz-juelich.de>
2014-10-19 14:51:29 +0200

    Added matplotlib animation examples
    These examples have slightly been modified.
    Matplotlib's animation module is no longer needed.
    The animation is now handled in explicit loops.
    
    Change-Id: I1201cfa7e3dc73b5f6e48dd5f7bb4442af53ee33
    Reviewed-on: https://iffwww.iff.kfa-juelich.de/review/98
    Reviewed-by: Josef Heinen <j.heinen@fz-juelich.de>

---
+++ examples/matplotlib/animation/animate_decay.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+fig, ax = plt.subplots()
+line, = ax.plot([], [], lw=2)
+ax.set_ylim(-1.1, 1.1)
+ax.set_xlim(0, 5)
+ax.grid()
+xdata, ydata = [], []
+
+
+def run(data):
+    # update the data
+    t, y = data
+    xdata.append(t)
+    ydata.append(y)
+    xmin, xmax = ax.get_xlim()
+
+    if t >= xmax:
+        ax.set_xlim(xmin, 2*xmax)
+        ax.figure.canvas.draw()
+    line.set_data(xdata, ydata)
+
+    return line,
+
+
+t = 0
+for cnt in range(1000): 
+    t += 0.05
+    run([t, np.sin(2*np.pi*t) * np.exp(-t/10.)])
+    plt.show()
---
+++ examples/matplotlib/animation/dynamic_image.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+An animated image
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+
+
+def f(x, y):
+    return np.sin(x) + np.cos(y)
+
+x = np.linspace(0, 2 * np.pi, 120)
+y = np.linspace(0, 2 * np.pi, 100).reshape(-1, 1)
+
+im = plt.imshow(f(x, y), cmap=plt.get_cmap('jet'))
+
+
+def updatefig(*args):
+    global x, y
+    x += np.pi / 15.
+    y += np.pi / 20.
+    im.set_array(f(x, y))
+    return im,
+
+for i in range(50):
+    updatefig()
+    plt.show()
