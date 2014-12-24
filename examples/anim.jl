cde511e92bbd86992d8d48412a8b341b1bed1dea
Josef Heinen <j.heinen@fz-juelich.de>
2014-12-24 07:35:11 +0100

    Improved Julia animation example
    Change-Id: Id325b6a76f3ba6646692853d3b38bdd5456afafe
    Reviewed-on: https://iffwww.iff.kfa-juelich.de/review/137
    Reviewed-by: Josef Heinen <j.heinen@fz-juelich.de>

--- examples/anim.jl
+++ examples/anim.jl
@@ -1,24 +1,33 @@
-using PyPlot
+# Compare line drawing performance of Matplotlib vs. GR
+#
+# These are the results on a MacBook Pro 2,6 GHz Intel Core i5::
+#
+#     fps (mpl):   27
+#     fps  (GR):  775
+#       speedup:   28.7
+
+import PyPlot
 
 x = [0:0.01:2*pi]
 
 tic()
-line, = plot(x, sin(x))
+line, = PyPlot.plot(x, sin(x))
 for i = 1:200
     line[:set_ydata](sin(x + i / 10.0))
-    draw()
+    PyPlot.draw()
 end
 
-fps_gr = int(200 / toc())
-println("fps  (PyPlot): ", fps_gr)
+fps_mpl = int(200 / toq())
+println("fps (mpl): ", fps_mpl)
 
-using jlgr
+import jlgr
 
 tic()
 for i = 1:200
-    plot(x, sin(x + i / 10.0))
+    jlgr.plot(x, sin(x + i / 10.0))
 end
 
-fps_gr = int(200 / toc())
+fps_gr = int(200 / toq())
 println("fps  (GR): ", fps_gr)
 
+println(@sprintf("  speedup: %6.1f",  float(fps_gr) / fps_mpl))
