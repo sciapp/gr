d8ca30ef624b49601f9fde2189a3d6c77967c0e5
Josef Heinen <j.heinen@fz-juelich.de>
2015-03-06 13:08:34 +0100

    Added BornAgain demo
    Change-Id: Ia38b51e25eb67e5ec4af5e06f7267928bc48a87d
    Reviewed-on: https://iffwww.iff.kfa-juelich.de/review/171
    Reviewed-by: Josef Heinen <j.heinen@fz-juelich.de>

---
+++ examples/bornagain-demo.py
@@ -0,0 +1,73 @@
+'''
+Simulation demo: movie of growing particles on substrate
+'''
+
+import os, sys, numpy
+import gr
+from gr.pygr import imshow
+
+from libBornAgainCore import *
+
+Nframes = 50
+
+radius = 1
+height = 4
+distance = 5
+
+# ----------------------------------
+# describe sample and run simulation
+# ----------------------------------
+def RunSimulation():
+    # defining materials
+    mAir = HomogeneousMaterial("Air", 0.0, 0.0)
+    mSubstrate = HomogeneousMaterial("Substrate", 6e-6, 2e-8)
+    mParticle = HomogeneousMaterial("Particle", 6e-4, 2e-8)
+
+    # collection of particles
+    cylinder_ff = FormFactorCylinder(radius, height)
+    cylinder = Particle(mParticle, cylinder_ff)
+    particle_layout = ParticleLayout()
+    particle_layout.addParticle(cylinder)
+
+    # interference function
+    interference = InterferenceFunctionRadialParaCrystal(distance)
+    pdf = FTDistribution1DGauss(3 * nanometer)
+    interference.setProbabilityDistribution(pdf)
+    particle_layout.addInterferenceFunction(interference)
+
+    # air layer with particles and substrate form multi layer
+    air_layer = Layer(mAir)
+    air_layer.addLayout(particle_layout)
+    substrate_layer = Layer(mSubstrate)
+    multi_layer = MultiLayer()
+    multi_layer.addLayer(air_layer)
+    multi_layer.addLayer(substrate_layer)
+
+    # build and run experiment
+    simulation = Simulation()
+    simulation.setDetectorParameters(100, -4.0 * degree, 4.0 * degree, 100, 0.0 * degree, 8.0 * degree)
+    simulation.setBeamParameters(1.0 * angstrom, 0.2 * degree, 0.0 * degree)
+    simulation.setSample(multi_layer)
+    simulation.runSimulation()
+    # intensity data
+    return simulation.getIntensityData().getArray()
+
+
+def SetParameters(i):
+    global radius
+    global height
+    global distance
+    radius = (1. + (3.0/Nframes)*i) * nanometer
+    height = (1. + (4.0/Nframes)*i) * nanometer
+    distance = (10. - (1.0/Nframes)*i) * nanometer
+
+#-------------------------------------------------------------
+# main()
+#-------------------------------------------------------------
+if __name__ == '__main__':
+    for i in range(Nframes):
+        SetParameters(i)
+        result = RunSimulation() + 1 # for log scale
+        result = numpy.log10(numpy.rot90(result, 1))
+        imshow(result, cmap=gr.COLORMAP_PILATUS)
+
