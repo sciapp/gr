6b26def8ae8b9974139287d19d322914f9914260
Josef Heinen <j.heinen@fz-juelich.de>
2015-03-10 15:41:26 +0100

    Improved anaconda script
    Change-Id: If99dd87afcbb599b6ef64dbfc1f0d685092f2386
    Reviewed-on: https://iffwww.iff.kfa-juelich.de/review/178
    Reviewed-by: Josef Heinen <j.heinen@fz-juelich.de>

--- bin/anaconda.sh
+++ bin/anaconda.sh
@@ -2,7 +2,14 @@
 if [ "$1" = "-o" ]
 then
   shift
-  export GKS_WSTYPE="$1"
+  if [ "`echo $1 | grep '\.'`" != "" ]
+  then
+    type=`echo $1 | awk -F. '{print $NF}'`
+    export GKS_FILEPATH="$1"
+    export GKS_WSTYPE="$type"
+  else
+    export GKS_WSTYPE="$1"
+  fi
   shift
 fi
 if [ $# -gt 0 ]
@@ -21,17 +28,19 @@ done
 name="${PWD}/$(basename "${0}")"
 cd "${cwd}"
 GRDIR=`dirname ${name} | sed -e 's;/bin;;'`
+opts=""
 if [ "${MPLBACKEND}" == "gr" ]
 then
   opts="-dmodule://gr.matplotlib.backend_gr"
-else
-  opts=""
 fi
 if [ -d /opt/anaconda ]
 then
   PYTHONHOME=/opt/anaconda
-else
+elif [ -d /usr/local/anaconda ]
+then
   PYTHONHOME=/usr/local/anaconda
+else
+  PYTHONHOME=/usr
 fi
 if [ `uname` = "Darwin" ]
 then
@@ -40,4 +49,8 @@ else
     export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${GRDIR}/lib
 fi
 export PYTHONPATH=${PYTHONPATH}:${GRDIR}/lib/python
+if [ -f ${GRDIR}/etc/grrc ]
+then
+  . ${GRDIR}/etc/grrc
+fi
 exec ${PYTHONHOME}/bin/python ${args} ${opts}
