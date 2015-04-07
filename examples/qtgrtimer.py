73ffe38659432afef57583f604e29666ba0ea38d
Christian Felder <c.felder@fz-juelich.de>
2015-04-07 15:20:22 +0200

    examples: Simple QtGR Example using a QTimer to update data.
    Change-Id: Ib0270334842e3912a31ec7798d4c97cb5a59beab
    Reviewed-on: https://iffwww.iff.kfa-juelich.de/review/210
    Reviewed-by: Josef Heinen <j.heinen@fz-juelich.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

---
+++ examples/qtgrtimer.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- no-plot -*-
+"""
+Plotting two-dimensional live data using QtGR.
+"""
+
+# standard library
+import sys
+setattr(sys, "QT_BACKEND_ORDER", ["PyQt4", "PySide"])
+# third party
+from PyQt4 import QtCore
+from PyQt4 import QtGui
+from numpy import arange, sin, pi
+# local library
+from gr.pygr import Plot, PlotAxes, PlotCurve
+from qtgr import InteractiveGRWidget
+
+__author__ = "Christian Felder <c.felder@fz-juelich.de>"
+__copyright__ = """Copyright 2012-2015 Forschungszentrum Juelich GmbH
+
+This file is part of GR, a universal framework for visualization applications.
+Visit http://gr-framework.org for the latest
+version.
+
+GR was developed by the Scientific IT-Systems group at the Peter Grünberg
+Institute at Forschunsgzentrum Jülich. The main development has been done
+by Josef Heinen who currently maintains the software.
+
+GR is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This framework is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GR. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        QtGui.QMainWindow.__init__(self, *args, **kwargs)
+        self._grw = InteractiveGRWidget()
+        self.setCentralWidget(self._grw)
+
+        viewport = [0.1, 0.88, 0.1, 0.88]
+        x = arange(0, 2 * pi, 0.01)
+        y = sin(x)
+
+        self.curve = PlotCurve(x, y)
+        axes = PlotAxes(viewport)
+        axes.addCurves(self.curve)
+        plot = Plot(viewport).addAxes(axes)
+        plot.title = "QtGR Timer example"
+        plot.subTitle = "Plotting Live-Data"
+        plot.xlabel = "t"
+        plot.ylabel = "sin(t)"
+        plot.autoscale = PlotAxes.SCALE_X | PlotAxes.SCALE_Y
+        self._grw.addPlot(plot)
+
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.updateData)
+        timer.start(40)
+
+    def updateData(self):
+        self.curve.x += 0.05
+        self.curve.y = sin(self.curve.x)
+        self.update()
+
+
+def main(*args):
+    app = QtGui.QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.resize(QtCore.QSize(500, 500))
+    mainWin.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main(sys.argv)
