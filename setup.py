4e8d5bf54861eb5404f3f08b2e972c7002385dfe
Georg Brandl <g.brandl@fz-juelich.de>
2015-09-30 09:33:52 +0200

    setup: build a shared lib for checking mupdf
    Debian/Ubuntu ships only a static libmupdf.a which is not PIC,
    so it succeeds in building the test binary, but not the shared lib later.
    
    Change-Id: I280f0ffd3fb6c62eac8a792a912b35077d8c2236
    Reviewed-on: https://iffwww.iff.kfa-juelich.de/review/387
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

--- setup.py
+++ setup.py
@@ -352,7 +352,7 @@ int main(int argc, char **argv)
 
 """)
         os.close(fd)
-        cmd = [self.cc, "-o", tmpout, tmpsrc]
+        cmd = [self.cc, "-fPIC", "-shared", "-o", tmpout, tmpsrc]
         cmd.extend(cflags)
         cmd.extend(mupdfcflags)
         cmd.extend(ldflags)
