e3f4351fe49c6ee160ae51881ffab543664bbba2
Josef Heinen <j.heinen@fz-juelich.de>
2015-09-30 11:32:59 +0200

    Changed license information
    Change-Id: I5d53b2f4bdcabf822efed9f509a255e338ba5ffc
    Reviewed-on: https://iffwww.iff.kfa-juelich.de/review/388
    Reviewed-by: Josef Heinen <j.heinen@fz-juelich.de>

--- examples/qtgrcontour.py
+++ examples/qtgrcontour.py
@@ -13,28 +13,36 @@ from qtgr.backend import QtCore, QtGui
 from qtgr import InteractiveGRWidget
 
 __author__ = "Christian Felder <c.felder@fz-juelich.de>"
-__copyright__ = """Copyright 2012-2015 Forschungszentrum Juelich GmbH
+__copyright__ = """Copyright (c) 2012-2015: Josef Heinen, Florian Rhiem,
+Christian Felder and other contributors:
 
-This file is part of GR, a universal framework for visualization applications.
-Visit http://gr-framework.org for the latest
-version.
+http://gr-framework.org/credits.html
 
-GR was developed by the Scientific IT-Systems group at the Peter Grünberg
-Institute at Forschunsgzentrum Jülich. The main development has been done
-by Josef Heinen who currently maintains the software.
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
 
-GR is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
 
-This framework is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-You should have received a copy of the GNU General Public License
-along with GR. If not, see <http://www.gnu.org/licenses/>.
+The GR framework can be built with plugins that use code from the
+following projects, which have their own licenses:
+
+- MuPDF - a lightweight PDF and XPS viewer (AFPL)
+- Ghostscript - an interpreter for the PostScript language and for PDF (AFPL)
+- FFmpeg - a multimedia framework (LGPL / GPLv2)
 
 """
 
