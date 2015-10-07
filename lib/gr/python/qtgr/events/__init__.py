528359a815a6b33aa50f43686f17bdf07364b2f8
Christian Felder <c.felder@fz-juelich.de>
2015-10-07 10:32:41 +0200

    qtgr: Refactor events module. Move mouse events into its own submodule.
    For convenience all events will be imported into the events module.
    
    Change-Id: I278610b78316a5cc20bf6061ae15102e8740bb5f
    Reviewed-on: https://iffwww.iff.kfa-juelich.de/review/408
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

--- lib/gr/python/qtgr/events/__init__.py
+++ lib/gr/python/qtgr/events/__init__.py
@@ -4,8 +4,8 @@
 import logging
 # local library
 from qtgr.backend import QtCore
-from qtgr.events.base import EventMeta, MouseLocationEventMeta
-from gr.pygr.base import GRViewPort
+from qtgr.events.mouse import MouseEvent, WheelEvent, PickEvent, LegendEvent,\
+    ROIEvent
 from gr._version import __version__, __revision__
 
 __author__ = "Christian Felder <c.felder@fz-juelich.de>"
@@ -36,93 +36,6 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 _log = logging.getLogger(__name__)
 
-class MouseEvent(MouseLocationEventMeta):
-
-    MOUSE_MOVE = QtCore.QEvent.registerEventType()
-    MOUSE_PRESS = QtCore.QEvent.registerEventType()
-    MOUSE_RELEASE = QtCore.QEvent.registerEventType()
-
-    NO_BUTTON = 0x0
-    LEFT_BUTTON = 0x1
-    RIGHT_BUTTON = 0x2
-
-    NO_MODIFIER = 0x00
-    SHIFT_MODIFIER = 0x02
-    CONTROL_MODIFIER = 0x04
-    ALT_MODIFIER = 0x08
-    META_MODIFIER = 0x10
-    KEYPAD_MODIFIER = 0x20
-    GROUP_SWITCH_MODIFIER = 0x40
-
-    def __init__(self, type, width, height, x, y, buttons, modifiers):
-        super(MouseEvent, self).__init__(type, width, height, x, y)
-        self._buttons = buttons
-        self._modifiers = modifiers
-
-    def type(self):
-        return self._type
-
-    def getButtons(self):
-        return self._buttons
-
-    def getModifiers(self):
-        return self._modifiers
-
-class WheelEvent(MouseLocationEventMeta):
-
-    WHEEL_MOVE = QtCore.QEvent.registerEventType()
-
-    def __init__(self, type, width, height, x, y, buttons, delta):
-        super(WheelEvent, self).__init__(type, width, height, x, y)
-        self._buttons = buttons
-        self._delta = delta
-
-    def getDelta(self):
-        return self._delta
-
-    def getDegree(self):
-        return self.getDelta() / 8.
-
-    def getSteps(self):
-        return self.getDegree() / 15.
-
-    def getButtons(self):
-        return self._buttons
-
-class PickEvent(MouseLocationEventMeta, GRViewPort):
-
-    PICK_MOVE = QtCore.QEvent.registerEventType()
-    PICK_PRESS = QtCore.QEvent.registerEventType()
-
-    def __init__(self, type, width, height, x, y, viewport, window=None):
-        super(PickEvent, self).__init__(type, width, height, x, y, window)
-        GRViewPort.__init__(self, viewport)
-
-class ROIEvent(MouseEvent):
-
-    ROI_CLICKED = QtCore.QEvent.registerEventType()
-    ROI_OVER = QtCore.QEvent.registerEventType()
-
-    def __init__(self, type, width, height, x, y, buttons, modifiers, roi):
-        super(ROIEvent, self).__init__(type, width, height, x, y, buttons,
-                                       modifiers)
-        self._roi = roi
-
-    @property
-    def roi(self):
-        """Get RegionOfInterest instance."""
-        return self._roi
-
-class LegendEvent(ROIEvent):
-
-    def __init__(self, type, width, height, x, y, buttons, modifiers, roi):
-        super(LegendEvent, self).__init__(type, width, height, x, y, buttons,
-                                          modifiers, roi)
-
-    @property
-    def curve(self):
-        """Get PlotCurve instance referenced by this legend item."""
-        return self._roi.reference
 
 class EventFilter(QtCore.QObject):
 
@@ -238,6 +151,7 @@ class CallbackManager(object):
 
         """
         return self._handler[type, cls]
+
 
 class GUIConnector(object):
 
