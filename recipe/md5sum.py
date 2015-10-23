751f7ac5e646b6e2059d2fe81415fba166127f91
Josef Heinen <j.heinen@fz-juelich.de>
2015-10-23 10:36:39 +0200

    Improved conda build scripts
    Change-Id: Icb46519a8daff60914484aa350e9c8c6ed8fc29b
    Reviewed-on: https://iffwww.iff.kfa-juelich.de/review/431
    Reviewed-by: Josef Heinen <j.heinen@fz-juelich.de>

--- recipe/md5sum.py
+++ recipe/md5sum.py
@@ -1,6 +1,10 @@
-import md5
+from __future__ import print_function
+
+import hashlib
 import sys
+
 path = sys.argv[1]
-m = md5.new()
+m = hashlib.md5()
 m.update(open(path, 'rb').read())
+
 print(m.hexdigest())
