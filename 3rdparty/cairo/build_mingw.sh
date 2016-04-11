8a083f5ffd938d29caff0006232a0110391497d4
Florian Rhiem <f.rhiem@fz-juelich.de>
2016-04-11 15:46:29 +0200

    Windows: Allow cross-compilation using mingw-w64
    Change-Id: I2f9b28da42629ee39e9d00814663af933f450e55
    Reviewed-on: https://iffwww.iff.kfa-juelich.de/review/563
    Reviewed-by: Josef Heinen <j.heinen@fz-juelich.de>

---
+++ 3rdparty/cairo/build_mingw.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+cwd=`pwd`
+src="cairo-1.14.6"
+if [ "$1" = "" ]; then
+  dest=`pwd`/../build
+else
+  dest=$1
+fi
+mkdir -p ${dest}/src
+cd ${dest}/src
+
+if [ ! -d "${src}" ]; then
+  if [ `which curl` ]; then
+    cmd="curl -O"
+  else
+    cmd="wget"
+  fi
+  ${cmd} http://cairographics.org/releases/${src}.tar.xz
+  tar -xf ${src}.tar.xz
+fi
+
+cd ${src}
+
+export CFLAGS=-I${dest}/../zlib
+export LIBS=${dest}/../zlib/libz.lib
+export png_REQUIRES=libpng16
+export png_CFLAGS=-I${dest}/../libpng16
+export png_LIBS=${dest}/../libpng16/libpng.lib
+export png_NONPKGCONFIG_CFLAGS=-I${dest}/../libpng16
+export png_NONPKGCONFIG_LIBS=${dest}/../libpng16/libpng.lib
+export pixman_CFLAGS=-I${dest}/include/pixman-1
+export pixman_LIBS=${dest}/lib/libpixman-1.a
+export MAKE=make
+
+./configure --prefix=${dest} --libdir=${dest}/lib \
+  --enable-static --disable-shared --with-pic --disable-quartz --disable-ft --host=x86_64-w64-mingw32 --disable-ps --disable-pdf --disable-interpreter
+make
+make install
+export MAKE="make -f makefile.mingw"
+cd ${cwd}
+ln -s ${dest}/src/${src}/src ${dest}/src/cairo
---
+++ 3rdparty/pixman/build_mingw.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+cwd=`pwd`
+src="pixman-0.34.0"
+if [ "$1" = "" ]; then
+  dest=`pwd`/../build
+else
+  dest=$1
+fi
+mkdir -p ${dest}/src
+cd ${dest}/src
+
+if [ ! -d "${src}" ]; then
+  if [ `which curl` ]; then
+    cmd="curl -O"
+  else
+    cmd="wget"
+  fi
+  ${cmd} http://cairographics.org/releases/${src}.tar.gz
+  tar -xf ${src}.tar.gz
+fi
+
+cd ${src}
+
+export MAKE=make
+./configure --prefix=${dest} --libdir=${dest}/lib \
+  --disable-shared --enable-static --with-pic --host=x86_64-w64-mingw32
+make
+make install
+export MAKE="make -f makefile.mingw"
+cd ${cwd}
