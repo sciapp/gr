988f5afccba2b4f3a44e8ed5742330ad1ae525b1
Florian Rhiem <f.rhiem@fz-juelich.de>
2016-04-19 15:30:18 +0200

    examples: modified orbitals.py to fit julia example
    Change-Id: Idf4df5676f59004ae670d7b42238abdffc93824a
    Reviewed-on: https://iffwww.iff.kfa-juelich.de/review/574
    Reviewed-by: Josef Heinen <j.heinen@fz-juelich.de>

--- examples/orbitals.py
+++ examples/orbitals.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 """
 Visualization of atomic orbitals using gr.pygr.mlab / GR3.
+
+Based on Atomic Orbitals Pt 2 by Christina C. Lee:
+https://albi3ro.github.io/M4/prerequisites/Atomic-Orbitals2.html
 """
 
 from __future__ import absolute_import
@@ -8,61 +11,45 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-
 import numpy as np
 from scipy.constants import physical_constants
-from scipy.special import eval_genlaguerre, lpmn
+from pygsl.testing import sf
 
 from gr.pygr.mlab import isosurface
 
-__author__ = 'Florian Rhiem <f.rhiem@fz-juelich.de>, Ingo Heimbach <i.heimbach@fz-juelich.de>'
+__author__ = 'Florian Rhiem <f.rhiem@fz-juelich.de>'
 
 
-def get_laguerre_polynomial(n, k):
-    return lambda x: eval_genlaguerre(n, k, x)
+def Yml(m, l, theta, phi):
+    """ The θ and ϕ dependence """
+    return (-1)**m * sf.legendre_Plm(l, abs(m), np.cos(theta)) * np.exp(1j * m * phi)
 
 
-def get_legendre_polynomial(n, k):
-    def P(x):
-        return np.array([lpmn(k, n, z)[0][-1, -1] for z in x])
-    return P
+def R(n, l, rho):
+    """ The radial dependence """
+    return sf.laguerre_n(n-l-1, 2*l+1, rho) * np.exp(-rho/2) * rho**l
 
 
-def get_radial_function(n, l):
-    a0 = physical_constants['Bohr radius'][0]
-    L = get_laguerre_polynomial(n-l-1, 2*l+1)
-    rho = lambda r: (2.0*r) / (n*a0)
-    R = lambda r: np.sqrt((2.0/(n*a0))**3 * factorial(n-l-1)/(2*n*factorial(n+l))) * np.exp(-rho(r)/2.0) * rho(r)**l * L(rho(r))
-    return R
-
-def get_spherical_harmonic_function(l, m):
-    P = get_legendre_polynomial(l, m)
-    if m < 0:
-        m = abs(m)
-        Y = lambda t, p: np.sqrt(2) * np.sqrt(((2.0*l+1)/(4*np.pi)) * ((1.0*factorial(l-m))/(factorial(l+m)))) * P(np.cos(t)) * np.sin(m*p)
-    elif m == 0:
-        Y = lambda t, p: np.sqrt(((2.0*l+1)/(4*np.pi)) * ((1.0*factorial(l-m))/(factorial(l+m)))) * P(np.cos(t))
-    else:
-        Y = lambda t, p: np.sqrt(2) * np.sqrt(((2.0*l+1)/(4*np.pi)) * ((1.0*factorial(l-m))/(factorial(l+m)))) * P(np.cos(t)) * np.cos(m*p)
-    return Y
+def norm(n, l):
+    """
+    A normalization:
+    This is dependent on the choice of polynomial representation
+    """
+    return np.sqrt((2/n)**3 * factorial(n-l-1)/(2*n*factorial(n+l)))
 
 
-def get_orbital_probability_density_function(n, l, m):
-    R = get_radial_function(n, l)
-    Y = get_spherical_harmonic_function(l, m)
+def orbital(n, l, m):
+    # we make sure l and m are within proper bounds
+    if l > n:
+        raise ValueError()
+    if abs(m) > l:
+        raise ValueError()
 
-    def R2(r):
-        res = R(r)
-        return (res*res.conjugate()).real
-
-    def Y2(t, p):
-        res = Y(t, p)
-        return (res*res.conjugate()).real
-
-    def probability_density_function(r, t, p):
-        return R2(r) * Y2(t, p)
-
-    return probability_density_function
+    def psi(radii, theta, phi):
+        a0 = physical_constants['Bohr radius'][0]
+        rho = 2*radii / (n*a0)
+        return norm(n, l) * R(n, l, rho) * np.abs(Yml(m, l, theta, phi))
+    return psi
 
 
 def factorial(n):
@@ -77,9 +64,10 @@ def to_spherical_coordinates(x, y, z):
 
 
 def main():
-    f = get_orbital_probability_density_function(3, 2, 0)
-    # create coordinates for box width sidelength 2*3
+    n, l, m = 3, 2, 0
+    f = orbital(n, l, m)
     r = 1e-9
+    # create coordinates for box width sidelength 2*3
     nx = ny = nz = 50
     coordinates = np.zeros((nz, ny, nx, 3))
     coordinates[:, :, :, 0] = np.linspace(-r, r, nx).reshape(1, 1, nx)
@@ -94,8 +82,8 @@ def main():
     values = f(radii, theta, phi).reshape(nz, ny, nx)
     # Show rotating orbital
     if values.max() > 0:
-        for alpha in np.linspace(0, 2*np.pi, 600):
-            isosurface(values/values.max(), isovalue=0.2, color=(0, 0.5, 0.8), camera_position=(2*np.sin(alpha), 1, 2*np.cos(alpha)))
+        for alpha in np.linspace(0, 2*np.pi, 300):
+            isosurface(values/values.max(), isovalue=0.25, color=(0, 0.5, 0.8), camera_position=(2*np.sin(alpha), 1, 2*np.cos(alpha)))
 
 if __name__ == '__main__':
     main()
