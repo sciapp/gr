sciapp/gr

commit 170441fed0ed20ebd4208c6975522f840a3b24de

Disabled unnecessary Cairo surfaces for self-contained build
Change-Id: I1e7e7bf899086927f9691f099c4e39d714b91486
Reviewed-on: https://iffwww.iff.kfa-juelich.de/review/633
Reviewed-by: Josef Heinen <j.heinen@fz-juelich.de>

3rdparty/cairo/build.sh
@@ -21,6 +21,8 @@ fi
 
 cd ${src}
 
+export CFLAGS=-I${dest}/../zlib
+export LIBS=${dest}/../zlib/libz.a
 export png_REQUIRES=libpng16
 export png_CFLAGS=-I${dest}/../libpng16
 export png_LIBS=${dest}/../libpng16/libpng.a
@@ -30,7 +32,8 @@ export pixman_CFLAGS=-I${dest}/include/pixman-1
 export pixman_LIBS=${dest}/lib/libpixman-1.a
 
 ./configure --prefix=${dest} --libdir=${dest}/lib \
-  --enable-static --with-pic --disable-quartz --disable-ft
+  --enable-static --with-pic --disable-quartz --disable-ft --disable-ps \
+  --disable-pdf --disable-interpreter
 make -j4
 make install
 

commit 678747aa22b9caf8c8eaae7c89edbfe9ee9f3821

Fix zlib path detection for cairo build.
Due to a regression in 170441fed0ed20ebd4208c6975522f840a3b24de
which makes assumptions about the ${dest} variable the zlib directory
path was wrong when ${dest} was set to a different respectively non-default
directory. The build script uses ${cwd} now to determine the correct path.

Change-Id: Id10f13b6ffc532502490034d9ae8b4e09c4e8195
Reviewed-on: https://iffwww.iff.kfa-juelich.de/review/866
Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

3rdparty/cairo/build.sh
@@ -22,8 +22,15 @@ fi
 
 cd ${src}
 
-export CFLAGS="-I${dest}/../zlib -O"
-export LIBS=${dest}/../zlib/libz.a
+export CFLAGS="-I${cwd}/../zlib -O"
+if [ -f "${cwd}/../zlib/libz.a" ]; then
+  export LIBS=${cwd}/../zlib/libz.a
+elif [ -f "${dest}/lib/libz.a" ]; then
+  export LIBS=${dest}/lib/libz.a
+else
+  echo "libz.a not found."
+  exit 1
+fi
 export png_REQUIRES=libpng16
 export png_CFLAGS=-I${dest}/../libpng16
 export png_LIBS=${dest}/../libpng16/libpng.a