d360a532b5c967633ff449afb103663ab184450b
Ingo Heimbach <i.heimbach@fz-juelich.de>
2017-12-19 17:30:34 +0100

    Removed references to the GR Python wrapper in packaging scripts

--- packaging/jenkins/compile_using_system_dependencies.sh
+++ packaging/jenkins/compile_using_system_dependencies.sh
@@ -5,7 +5,7 @@ sudo yum install -y gcc gcc-c++ mesa-libGL-devel
 else
 sudo yum update -y
 sudo yum install -y gcc gcc-c++ cmake patch
-sudo yum install -y PyQt4-devel ghostscript-devel texlive-collection-latex texlive-dvipng cairo-devel
+sudo yum install -y ghostscript-devel texlive-collection-latex texlive-dvipng cairo-devel
 fi
 fi
 
@@ -16,7 +16,7 @@ sudo yum install -y gcc gcc-c++ mesa-libGL-devel
 else
 sudo yum update -y
 sudo yum install -y gcc gcc-c++ cmake patch
-sudo yum install -y PyQt4-devel ghostscript-devel texlive-collection-latex texlive-dvipng cairo-devel
+sudo yum install -y ghostscript-devel texlive-collection-latex texlive-dvipng cairo-devel
 fi
 fi
 
@@ -24,24 +24,24 @@ if grep "Debian" /etc/*-release; then
 sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y update
 sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y dist-upgrade
 sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y install make gcc g++
-sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y install libx11-dev libxft-dev libxt-dev python2.7-dev python-numpy
-sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y install texlive-latex3 dvipng python-opengl libgl1-mesa-dev qt4-dev-tools pyqt4-dev-tools libgs-dev
-sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y install libgtk2.0-dev libwxgtk3.0-dev python-wxgtk3.0 libglfw3-dev libzmq3-dev python-pyaudio cmake
+sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y install libx11-dev libxft-dev libxt-dev python2.7
+sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y install texlive-latex3 dvipng libgl1-mesa-dev qt4-dev-tools libgs-dev
+sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y install libgtk2.0-dev libwxgtk3.0-dev libglfw3-dev libzmq3-dev cmake
 fi
 
 if grep "Ubuntu" /etc/*-release; then
 sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y update
 sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y dist-upgrade
 sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y install make gcc g++
-sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y install libx11-dev libxft-dev libxt-dev python2.7-dev python-numpy
-sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y install texlive-latex3 dvipng python-opengl libgl1-mesa-dev qt4-dev-tools pyqt4-dev-tools libgs-dev
-sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y install libgtk2.0-dev libwxgtk3.0-dev python-wxgtk3.0 libglfw3-dev libzmq3-dev python-pyaudio cmake
+sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y install libx11-dev libxft-dev libxt-dev python2.7
+sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y install texlive-latex3 dvipng libgl1-mesa-dev qt4-dev-tools libgs-dev
+sudo apt-get -qq -o=Dpkg::Use-Pty=0 -y install libgtk2.0-dev libwxgtk3.0-dev libglfw3-dev libzmq3-dev cmake
 fi
 
 if grep "Fedora 23" /etc/*-release; then
 sudo dnf -y upgrade
 sudo dnf -y install gcc gcc-c++
-sudo dnf -y install PyQt4-devel ghostscript-devel texlive-collection-latex texlive-dvipng zeromq3-devel glfw-devel mupdf-devel wxGTK-devel wxPython gtk2-devel
+sudo dnf -y install ghostscript-devel texlive-collection-latex texlive-dvipng zeromq3-devel glfw-devel mupdf-devel wxGTK-devel gtk2-devel
 fi
 
-make install GRDIR=/tmp/gr PYTHONBIN=/usr/local/bin/
+make install GRDIR=/tmp/gr
--- packaging/osx/build-whl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-version=`git ls-remote --tags https://github.com/jheinen/gr| grep -o 'v[0-9]*\.[0-9]*\.[0-9]*' | sort -t. -k 1,1n -k 2,2n -k 3,3n | tail -1`
-git clone -b ${version} https://github.com/jheinen/gr
-PYTHON=/usr/local/bin/python
-cd gr
-${PYTHON} setup.py build_ext --static-extras --disable-gtk --disable-xt --disable-wx bdist_wheel
