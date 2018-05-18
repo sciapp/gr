b24906131aff691d2bdb5316ae11a42334cacc34
Ingo Heimbach <i.heimbach@fz-juelich.de>
2018-05-18 08:54:57 +0200

    Ported line plots of the Python mlab layer to `meta.c` (partially)
    Added a new public routine `gr_plotmeta` that creates a plot from a
    given argument container. Currently, only line plots are supported.
    Colors and labels can be configured. See `lib/gr/meta/plot.c` for an
    example how to pass data and meta data.

--- lib/gr/gr.h
+++ lib/gr/gr.h
@@ -191,6 +191,7 @@ DLLEXPORT int gr_sendmeta_buf(const void *, const char *, const void *, int);
 DLLEXPORT int gr_sendmeta_ref(const void *, const char *, char, const void *, int);
 DLLEXPORT int gr_sendmeta_args(const void *p, const gr_meta_args_t *);
 DLLEXPORT void gr_closemeta(const void *);
+DLLEXPORT void gr_plotmeta(const gr_meta_args_t *);
 #ifndef NDEBUG
 DLLEXPORT void gr_dumpmeta(const gr_meta_args_t *, FILE *);
 DLLEXPORT void gr_dumpmeta_json(const gr_meta_args_t *, FILE *);
