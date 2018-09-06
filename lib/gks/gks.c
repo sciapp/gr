ff2a90974fd4678f9b154da533b37dec94449003
Florian Rhiem <f.rhiem@fz-juelich.de>
2018-09-06 14:16:54 +0200

    Added RGBA output to memory to cairo plugin

--- lib/gks/gks.c
+++ lib/gks/gks.c
@@ -57,6 +57,7 @@ ws_descr_t ws_types[] = {
   { 140, GKS_K_METERS, 0.28575, 0.19685, 6750, 4650, 0, "png", NULL },
   { 141, GKS_K_METERS, 0.25400, 0.19050, 1024,  768, 0, NULL, NULL },
   { 142, GKS_K_METERS, 0.25400, 0.19050, 1024,  768, 0, NULL, NULL },
+  { 143, GKS_K_METERS, 0.28575, 0.19685, 6750, 4650, 0, NULL, NULL },
   { 150, GKS_K_METERS, 0.20320, 0.15240,  560,  420, 0, "six", NULL },
   { 210, GKS_K_METERS, 0.33300, 0.28100, 1024,  864, 0, NULL, NULL },
   { 211, GKS_K_METERS, 0.33300, 0.28100, 1024,  864, 2, NULL, NULL },
@@ -324,7 +325,8 @@ void gks_ddlk(
 
 	    case 140:
 	    case 141:
-	    case 142:
+      case 142:
+      case 143:
 	    case 150:
 	      gks_cairo_plugin(fctid, dx, dy, dimx, i_arr,
 		len_f_arr_1, f_arr_1, len_f_arr_2, f_arr_2, len_c_arr, c_arr,
