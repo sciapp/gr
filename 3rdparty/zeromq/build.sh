83f46763552cdc7130d1a2cef22220ffc56171b7
Florian Rhiem <f.rhiem@fz-juelich.de>
2018-11-12 13:28:26 +0100

    Avoid clock_gettime in zeromq for 10.11 compatibility

--- 3rdparty/zeromq/build.sh
+++ 3rdparty/zeromq/build.sh
@@ -26,6 +26,10 @@ export CFLAGS=-fPIC
 export CXXFLAGS=-fPIC
 opts="SUBDIRS=src"
 ./configure --prefix=${dest} --libdir=${dest}/lib --disable-shared
+# Disable clock_gettime for macOS 10.11 compatibility
+echo '#ifdef HAVE_CLOCK_GETTIME' >> src/platform.hpp
+echo '#undef HAVE_CLOCK_GETTIME' >> src/platform.hpp
+echo '#endif' >> src/platform.hpp
 make ${opts} -j4
 make ${opts} install
 
