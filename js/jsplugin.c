e49fb2804864f58b5b89302fb9730b3488be7632
Ingo Heimbach <i.heimbach@fz-juelich.de>
2020-01-29 16:10:33 +0100

    Fix C/C++ code formatting for clang-format version 9
    `clang-format` added a new format attribute for breaking braces after
    case labels. This attribute must be set to `true` to keep the coding
    style.

--- js/jsplugin.c
+++ js/jsplugin.c
@@ -14,7 +14,9 @@ typedef struct JS_point_t
 } JS_point;
 
 #ifdef __cplusplus
-#define extern "C" {
+#define extern \
+  "C"          \
+  {
 #endif
 
 extern void js_stroke(int n, JS_point *points, unsigned char color[4], int linewidth);
