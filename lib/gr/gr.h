95c3ef3188db4af381ff33f5cca3f7142a3a2d64
Yusuke Kominami <komi@Yusuke.local>
2020-04-18 16:30:21 +0900

    added gr_inqcharheight to lib/gr/gr.h

--- lib/gr/gr.h
+++ lib/gr/gr.h
@@ -64,6 +64,7 @@ DLLEXPORT void gr_setcharspace(double);
 DLLEXPORT void gr_settextcolorind(int);
 DLLEXPORT void gr_inqtextcolorind(int *);
 DLLEXPORT void gr_setcharheight(double);
+DLLEXPORT void gr_inqcharheight(double *);
 DLLEXPORT void gr_setcharup(double, double);
 DLLEXPORT void gr_settextpath(int);
 DLLEXPORT void gr_settextalign(int, int);
