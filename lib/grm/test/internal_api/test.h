436dee9010b6691445a3cfbaef9f361215c90c42
Ingo Heimbach <i.heimbach@fz-juelich.de>
2020-05-05 17:43:42 +0200

    [GRM] Add a test for the `get_compatible_format` function
    Add a test for the `get_compatible_format` function and generally
    restructure the test modules.

---
+++ lib/grm/test/internal_api/test.h
@@ -0,0 +1,34 @@
+#ifndef TEST_H_INCLUDED
+#define TEST_H_INCLUDED
+
+#include <assert.h>
+#include <stdio.h>
+
+#ifndef NDEBUG
+#define DEFINE_TEST_MAIN                                \
+  int main(void)                                        \
+  {                                                     \
+    test();                                             \
+                                                        \
+    if (isatty(fileno(stdout)))                         \
+      {                                                 \
+        printf("\033[32;1mAll tests passed!\033[0m\n"); \
+      }                                                 \
+    else                                                \
+      {                                                 \
+        printf("All tests passed!\n");                  \
+      }                                                 \
+                                                        \
+    return 0;                                           \
+  }
+#else
+#define DEFINE_TEST_MAIN                                \
+  int main(void)                                        \
+  {                                                     \
+    fprintf(stderr, "Please compile in debug mode!\n"); \
+                                                        \
+    return 1;                                           \
+  }
+#endif
+
+#endif /* ifndef TEST_H_INCLUDED */
