1d358fcb1d5c3f1aa139f524468f3da28212d9f3
Daniel Kaiser <d.kaiser@fz-juelich.de>
2020-11-18 14:17:57 +0100

    Add debian 9 based armhf cross compile build

---
+++ cmake/armhf-linux-gnu.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR armhf)
+
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+set(CMAKE_LINKER arm-linux-gnueabihf-ld)
+set(CMAKE_STRIP arm-linux-gnueabihf-strip)
+set(CMAKE_FIND_ROOT_PATH "/usr/lib/arm-linux-gnueabihf;/usr/arm-linux-gnueabihf")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
