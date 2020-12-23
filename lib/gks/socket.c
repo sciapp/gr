sciapp/gr

commit 226df00bf6aa683dc60ef71f73a83a93f36f6a7d

Fix implicit declarations in C90 mode

lib/gks/socket.c
@@ -1,3 +1,6 @@
+#ifdef __unix__
+#define _POSIX_C_SOURCE 200809L
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -188,7 +191,10 @@ static int open_socket(int wstype)
                 }
             }
 #ifndef _WIN32
-          usleep(300000);
+          {
+            struct timespec delay = {300, 0};
+            nanosleep(&delay, NULL);
+          }
 #else
           Sleep(300);
 #endif

lib/gks/gks.c
@@ -1,3 +1,6 @@
+#ifdef __unix__
+#define _POSIX_C_SOURCE 1
+#endif
 
 #include <ctype.h>
 #include <stdio.h>

lib/gks/gksforbnd.c
@@ -1,3 +1,6 @@
+#ifdef __unix__
+#define _XOPEN_SOURCE
+#endif
 
 #include <stdio.h>
 #include <string.h>

lib/gks/plugin/cairoplugin.c
@@ -118,6 +118,10 @@ DLLEXPORT void gks_cairoplugin(int fctid, int dx, int dy, int dimx, int *i_arr, 
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 #endif
 
+#ifndef round
+#define round(x) (((x) < 0) ? ceil((x)-.5) : floor((x) + .5))
+#endif
+
 
 /* set this flag so that the exit handler won't try to use Cairo X11 support */
 static int exit_due_to_x11_support_ = 0;
@@ -530,7 +534,7 @@ static void fill_routine(int n, double *px, double *py, int tnr)
       pattern = cairo_pattern_create_for_surface(image);
       cairo_pattern_set_extend(pattern, CAIRO_EXTEND_REPEAT);
       cairo_pattern_set_filter(pattern, CAIRO_FILTER_NEAREST);
-      cairo_matrix_init_scale(&pattern_matrix, 500.0 / fmin(p->width, p->height), 500.0 / fmin(p->width, p->height));
+      cairo_matrix_init_scale(&pattern_matrix, 500.0 / min(p->width, p->height), 500.0 / min(p->width, p->height));
       cairo_pattern_set_matrix(pattern, &pattern_matrix);
 
       cairo_set_source(p->cr, pattern);
@@ -583,7 +587,7 @@ static void polyline(int n, double *px, double *py)
   set_color(ln_color);
 
   gks_get_dash_list(ln_type, ln_width, gks_dashes);
-  for (i = 0; i < gks_dashes[0]; i++) p->dashes[i] = gks_dashes[i + 1] * fmin(p->width, p->height) / 500.0;
+  for (i = 0; i < gks_dashes[0]; i++) p->dashes[i] = gks_dashes[i + 1] * min(p->width, p->height) / 500.0;
   cairo_set_dash(p->cr, p->dashes, gks_dashes[0], 0);
 
   gks_set_dev_xform(gkss, p->window, p->viewport);
@@ -2126,7 +2130,7 @@ void gks_cairoplugin(int fctid, int dx, int dy, int dimx, int *ia, int lr1, doub
             {
               p->width = p->viewport[1] * p->w / p->mw;
               p->height = p->viewport[3] * p->h / p->mh;
-              p->nominal_size = fmin(p->width, p->height) / 500.0;
+              p->nominal_size = min(p->width, p->height) / 500.0;
             }
           close_page();
           open_page();

lib/gks/plugin/glplugin.c
@@ -454,8 +454,8 @@ static void draw_marker(double xn, double yn, int mtype, double mscale, int mcol
         case 8: /* hollow arc */
           {
             num_segments = 4 * r;
-            c = cosf(2 * M_PI / (num_segments - 1));
-            s = sinf(2 * M_PI / (num_segments - 1));
+            c = cos(2 * M_PI / (num_segments - 1));
+            s = sin(2 * M_PI / (num_segments - 1));
             xr = r;
             yr = 0;
             if (op == 7)

lib/gks/plugin/gsplugin.c
@@ -1,3 +1,6 @@
+#ifdef __unix__
+#define _POSIX_C_SOURCE 200112L
+#endif
 
 #ifndef NO_GS
 

lib/gks/plugin/wmfplugin.c
@@ -1,3 +1,6 @@
+#ifdef __unix__
+#define _POSIX_C_SOURCE 1
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>

lib/gks/plugin/x11plugin.c
@@ -1,3 +1,6 @@
+#ifdef __unix__
+#define _POSIX_C_SOURCE 199506L
+#endif
 
 #include <stdio.h>
 
@@ -36,10 +39,6 @@ DLLEXPORT void gks_x11plugin(int fctid, int dx, int dy, int dimx, int *i_arr, in
 
 #if !defined(VMS) && !defined(_WIN32)
 #include <unistd.h>
-#endif
-
-#ifdef __osf__
-int usleep(useconds_t);
 #endif
 
 #ifdef XSHM
@@ -3599,13 +3598,12 @@ static void get_pointer(int *n, double *x, double *y, int *state, int *term)
       *state = Undefined;
       inc = 1;
 
-      if (p->wstype == 212)
 #ifndef _WIN32
-#ifdef __osf__
-        usleep(200000); /* 200 ms */
-#else
-        sleep(1);
-#endif
+      if (p->wstype == 212)
+        {
+          struct timespec delay = {0, 200000000}; /* 200 ms */
+          nanosleep(&delay, NULL);
+        }
 #endif
 
       do
@@ -3954,7 +3952,10 @@ static void *event_loop(void *arg)
   p->run = 1;
   while (p->run)
     {
-      usleep(10000);
+      {
+        struct timespec delay = {0, 10000000}; /* 10 ms */
+        nanosleep(&delay, NULL);
+      }
 
       if (idle && p->run)
         {
@@ -4184,7 +4185,10 @@ void gks_x11plugin(int fctid, int dx, int dy, int dimx, int *ia, int lr1, double
               while (!p->done)
                 {
                   p->run = 0;
-                  usleep(10000);
+                  {
+                    struct timespec delay = {0, 10000000}; /* 10 ms */
+                    nanosleep(&delay, NULL);
+                  }
                 }
             }
         }

commit 6fc55d76761c7855c29060b9507fd62e2ca52d38

Fix a wrong sleep interval in `socket.c`
The changes in commit 226df00b accidentally increased the sleep interval
in `socket.c` from 300 ms to 300 s. This commit restores the old sleep
interval.

lib/gks/socket.c
@@ -192,7 +192,7 @@ static int open_socket(int wstype)
             }
 #ifndef _WIN32
           {
-            struct timespec delay = {300, 0};
+            struct timespec delay = {0, 300000000};
             nanosleep(&delay, NULL);
           }
 #else