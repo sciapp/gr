e285acd3499f79c43045b15f34261af391be2a7f
Josef Heinen <j.heinen@fz-juelich.de>
2021-12-17 13:11:14 +0100

    GKS: reduce resolution for iTerm graphics

--- lib/gks/plugin/cairoplugin.c
+++ lib/gks/plugin/cairoplugin.c
@@ -2205,7 +2205,7 @@ void gks_cairoplugin(int fctid, int dx, int dy, int dimx, int *ia, int lr1, doub
 #endif
       p->mem = NULL;
 
-      if (p->wtype == 140 || p->wtype == 144 || p->wtype == 145 || p->wtype == 146 || p->wtype == 151)
+      if (p->wtype == 140 || p->wtype == 144 || p->wtype == 145 || p->wtype == 146)
         {
           p->mw = 0.28575;
           p->mh = 0.19685;
@@ -2271,6 +2271,16 @@ void gks_cairoplugin(int fctid, int dx, int dy, int dimx, int *ia, int lr1, doub
           p->dpi = 100;
           resize(400, 400);
           p->nominal_size = 0.8;
+        }
+      else if (p->wtype == 151)
+        {
+          p->mw = 0.28575;
+          p->mh = 0.19685;
+          p->w = 1920;
+          p->h = 1440;
+          p->dpi = 200;
+          resize(500, 500);
+          p->nominal_size = 1;
         }
       else
         {
