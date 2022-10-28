3baf7ca433f51713145b1df790951057661600ae
Ole Köpke <o.koepke@fz-juelich.de>
2022-10-28 11:46:01 +0200

    [GRM] Panzoom example added

---
+++ lib/grm/test/public_api/grm/panzoom.c
@@ -0,0 +1,53 @@
+#ifdef __unix__
+#define _XOPEN_SOURCE 500
+#endif
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "grm.h"
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+
+static void test_panzoom(void)
+{
+    double plots[2][1000];
+    int n = sizeof(plots[0]) / sizeof(plots[0][0]);
+    grm_args_t *args;
+    int i;
+
+    printf("filling argument container...\n");
+
+    for (i = 0; i < n; ++i)
+    {
+        plots[0][i] = i * 2 * M_PI / n;
+        plots[1][i] = 2 * sin(i * 2 * M_PI / n);
+    }
+
+    args = grm_args_new();
+
+    grm_args_push(args, "x", "nD", n, plots[0]);
+    grm_args_push(args, "y", "nD", n, plots[1]);
+    grm_plot(args);
+    printf("Press any key to continue...\n");
+    getchar();
+
+    grm_args_push(args, "panzoom", "dddd", -0.04, 0.0, 0.8, 1.0);
+    grm_plot(args);
+    printf("Press any key to continue...\n");
+    getchar();
+
+    grm_args_delete(args);
+}
+
+int main(void)
+{
+    test_panzoom();
+
+    return 0;
+}
+
+
