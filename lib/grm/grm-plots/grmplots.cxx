38e9368d7a5ffbb06329af49fd34864b595793e4
Verbov <t.verbovsek@fz-juelich.de>
2022-11-30 13:37:15 +0100

    Changed the console arguments to key-value pairs

--- lib/grm/grm-plots/grmplots.cxx
+++ lib/grm/grm-plots/grmplots.cxx
@@ -1,26 +1,37 @@
 #include "grmplots_mainwindow.hxx"
 #include <QApplication>
 
+bool string_starts_with(const std::string &str, const std::string &prefix)
+{
+  return str.size() >= prefix.size() && 0 == str.compare(0, prefix.size(), prefix);
+}
+
 int main(int argc, char **argv)
 {
+  unsigned int i;
+  std::string token;
   const char *colms = "", *csv_file = "", *plot_type = "line";
 
-  if (argc > 1)
+  for (i = 1; i < argc; i++)
     {
-      csv_file = argv[1];
+      token = argv[i];
+      if (string_starts_with(token, "file:"))
+        {
+          csv_file = token.substr(5, token.length() - 1).c_str();
+        }
+      else if (string_starts_with(token, "type:"))
+        {
+          plot_type = token.substr(5, token.length() - 1).c_str();
+        }
+      else if (string_starts_with(token, "columns:"))
+        {
+          colms = token.substr(8, token.length() - 1).c_str();
+        }
     }
-  else
+  if (strcmp(csv_file, "") == 0)
     {
       fprintf(stderr, "Please specify a file to run grm-plots.\n");
       exit(0);
-    }
-  if (argc > 2)
-    {
-      plot_type = argv[2];
-    }
-  if (argc > 3)
-    {
-      colms = argv[3];
     }
 
   QApplication app(argc, argv);
