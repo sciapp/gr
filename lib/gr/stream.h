fdefd83fe716ebe5fd128fe97b721927c802e651
Josef Heinen <j.heinen@fz-juelich.de>
2023-04-25 16:49:03 +0200

    Added gr_startlistener() function

--- lib/gr/stream.h
+++ lib/gr/stream.h
@@ -9,6 +9,7 @@ int gr_openstream(const char *path);
 void gr_writestream(char *string, ...);
 void gr_flushstream(int discard);
 void gr_closestream(void);
+int gr_startlistener(void);
 
 #ifdef __cplusplus
 }
