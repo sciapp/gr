68f2a040959f8c5e1d00e2b02656c56aa6a19771
Du Kim Nguyen <k.nguyen@fz-juelich.de>
2023-10-18 16:20:30 +0200

    fixes issue with yrange without lim

--- lib/grm/src/grm/dom_render/render.cxx
+++ lib/grm/src/grm/dom_render/render.cxx
@@ -6375,6 +6375,7 @@ static void processPolarAxes(const std::shared_ptr<GRM::Element> &element, const
   if (kind == "polar_heatmap" || kind == "nonuniformpolar_heatmap")
     {
       r_min = static_cast<double>(subplotElement->getAttribute("r_min"));
+      r_min = 0;
       r_max = static_cast<double>(subplotElement->getAttribute("r_max"));
     }
   else
@@ -6437,7 +6438,9 @@ static void processPolarAxes(const std::shared_ptr<GRM::Element> &element, const
         }
       else
         {
-          tick = auto_tick(r_min, r_max);
+          tick = auto_tick_rings_polar(r_max, rings, norm);
+          subplotElement->setAttribute("r_max", tick * rings);
+          subplotElement->setAttribute("rings", rings);
         }
     }
 
@@ -8030,7 +8033,9 @@ static void processPolarHeatmap(const std::shared_ptr<GRM::Element> &element,
             }
           if (i < rows)
             {
-              rho.push_back(y_min + y_vec[i] / (y_max - y_min));
+              //              rho.push_back(y_min + y_vec[i] / (y_max - y_min)); // not needed since the data is already
+              //              transformed
+              rho.push_back(y_vec[i] / y_max);
             }
         }
 
