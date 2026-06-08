sciapp/gr

commit b7520a3c05b547f8f14e92925d9aecc20a1ef909

fix for tool-button tooltips that weren't visible; add option in icon bar for draw_grid attribute so it the grid can be removed easier; fix for visual different pixmap after grm_export; small adjustments for some text colors in titles3d and axes3d so that they don't get affected by other elements; fix export in multiplot case since only the active plot got exported cause the background didn't got applied; make sure checkstate in menubar fit to the plot state in multiplot case since it can differ between different plots; removed duplication in code which get caused by wrong merge; fixed a wrong coordinate_system which got created if the user wanted to use a logarithmic view after a panzoom got applied in the same run, caused by an unwanted reset to the non log lims in processLimits which wasn't needed since the lims get freshly calculated after the swap; added a missing check for multiplots when selecting the plot in GRPlotWidget; fix visual selection for menu in multiplot case; cleanup; remove some more updates that shouldn't be triggered since there is no change; removed unneeded resize and center triggered by dock widgets (show, hide); if log gets disabled reset data vecs with org data so that values <= 0 are visible again; add a special case for log-data contexts to reduce recreations; get rid of some unneeded updates from comboboxes; always clear cache after EditElement got accepted; fix for double plots after saving

lib/grm/src/grm/plot.cxx
@@ -6814,9 +6814,19 @@ int grm_process_tree(void)
 
 int grm_export(const char *file_path, int export_xml)
 {
+  auto active_figure = global_root->querySelectors("figure[active=\"1\"]");
+  auto active_plot = active_figure->querySelectors("plot[_active=\"1\"]");
+  auto active_plot_through_update = active_figure->querySelectors("plot[_active_through_update=\"1\"]");
+
+  if (active_plot != nullptr) active_plot->setAttribute("_active", 0);
+  if (active_plot_through_update != nullptr) active_plot_through_update->setAttribute("_active_through_update", 0);
+
   gr_beginprint(const_cast<char *>(file_path));
   int return_value = grm_plot(nullptr);
   gr_endprint();
+
+  if (active_plot != nullptr) active_plot->setAttribute("_active", 1);
+  if (active_plot_through_update != nullptr) active_plot_through_update->setAttribute("_active_through_update", 1);
 
   if (export_xml)
     {

cmake/ImportIconImages.cmake
@@ -10,6 +10,7 @@ set(GRPLOT_ICONS
     kind
     polar_with_pan
     text_color_ind
+    disable_grid
     use_gr3_dark
     algorithm_dark
     aspect_ratio_dark
@@ -21,6 +22,7 @@ set(GRPLOT_ICONS
     kind_dark
     polar_with_pan_dark
     text_color_ind_dark
+    disable_grid_dark
 )
 set(GRPLOT_ICON_QRC "<RCC>\n    <qresource prefix=\"/\">\n")
 foreach(icon IN LISTS GRPLOT_ICONS)

lib/grm/grplot/GRPlotMainWindow.cxx
@@ -264,132 +264,174 @@ void GRPlotMainWindow::resizeGRPlotWidget(int w, int h)
 
 void GRPlotMainWindow::showEditElementDockSlot()
 {
-  auto w = grplot_widget_->width();
-  auto h = grplot_widget_->height();
-  edit_element_dock_widget->show();
-  edit_element_dock_widget->setWindowTitle(grplot_widget_->getEditElementWidget()->windowTitle());
-  resizeGRPlotWidget(w, h);
-  center();
+  if (!edit_element_dock_widget->isVisible())
+    {
+      auto w = grplot_widget_->width();
+      auto h = grplot_widget_->height();
+      edit_element_dock_widget->show();
+      edit_element_dock_widget->setWindowTitle(grplot_widget_->getEditElementWidget()->windowTitle());
+      resizeGRPlotWidget(w, h);
+      center();
+    }
 }
 
 void GRPlotMainWindow::showTreeWidgetDockSlot()
 {
-  auto w = grplot_widget_->width();
-  auto h = grplot_widget_->height();
-  tree_dock_widget->show();
-  resizeGRPlotWidget(w, h);
-  center();
+  if (!tree_dock_widget->isVisible())
+    {
+      auto w = grplot_widget_->width();
+      auto h = grplot_widget_->height();
+      tree_dock_widget->show();
+      resizeGRPlotWidget(w, h);
+      center();
+    }
 }
 
 void GRPlotMainWindow::showTableWidgetDockSlot()
 {
-  auto w = grplot_widget_->width();
-  auto h = grplot_widget_->height();
-  table_dock_widget->show();
-  resizeGRPlotWidget(w, h);
-  center();
+  if (!table_dock_widget->isVisible())
+    {
+      auto w = grplot_widget_->width();
+      auto h = grplot_widget_->height();
+      table_dock_widget->show();
+      resizeGRPlotWidget(w, h);
+      center();
+    }
 }
 
 void GRPlotMainWindow::showTextPreviewDockSlot()
 {
-  auto w = grplot_widget_->width();
-  auto h = grplot_widget_->height();
-  text_preview_dock_widget->show();
-  resizeGRPlotWidget(w, h);
-  center();
+  if (!text_preview_dock_widget->isVisible())
+    {
+      auto w = grplot_widget_->width();
+      auto h = grplot_widget_->height();
+      text_preview_dock_widget->show();
+      resizeGRPlotWidget(w, h);
+      center();
+    }
 }
 
 void GRPlotMainWindow::showSelectionListDockSlot()
 {
-  auto w = grplot_widget_->width();
-  auto h = grplot_widget_->height();
-  selection_list_dock_widget->show();
-  resizeGRPlotWidget(w, h);
-  center();
+  if (!selection_list_dock_widget->isVisible())
+    {
+      auto w = grplot_widget_->width();
+      auto h = grplot_widget_->height();
+      selection_list_dock_widget->show();
+      resizeGRPlotWidget(w, h);
+      center();
+    }
 }
 
 void GRPlotMainWindow::showIconBarDockSlot()
 {
-  auto w = grplot_widget_->width();
-  auto h = grplot_widget_->height();
-  icon_bar_dock_widget->show();
-  resizeGRPlotWidget(w, h);
-  center();
+  if (!icon_bar_dock_widget->isVisible())
+    {
+      auto w = grplot_widget_->width();
+      auto h = grplot_widget_->height();
+      icon_bar_dock_widget->show();
+      resizeGRPlotWidget(w, h);
+      center();
+    }
 }
 
 void GRPlotMainWindow::showAddElementDockSlot()
 {
-  auto w = grplot_widget_->width();
-  auto h = grplot_widget_->height();
-  add_element_dock_widget->show();
-  add_element_dock_widget->setWindowTitle(grplot_widget_->getAddElementWidget()->windowTitle());
-  resizeGRPlotWidget(w, h);
-  center();
+  if (!add_element_dock_widget->isVisible())
+    {
+      auto w = grplot_widget_->width();
+      auto h = grplot_widget_->height();
+      add_element_dock_widget->show();
+      add_element_dock_widget->setWindowTitle(grplot_widget_->getAddElementWidget()->windowTitle());
+      resizeGRPlotWidget(w, h);
+      center();
+    }
 }
 
 void GRPlotMainWindow::hideEditElementDockSlot()
 {
-  auto w = grplot_widget_->width();
-  auto h = grplot_widget_->height();
-  edit_element_dock_widget->hide();
-  edit_element_dock_widget->setWindowTitle(grplot_widget_->getEditElementWidget()->windowTitle());
-  resizeGRPlotWidget(w, h);
-  center();
+  if (edit_element_dock_widget->isVisible())
+    {
+      auto w = grplot_widget_->width();
+      auto h = grplot_widget_->height();
+      edit_element_dock_widget->hide();
+      edit_element_dock_widget->setWindowTitle(grplot_widget_->getEditElementWidget()->windowTitle());
+      resizeGRPlotWidget(w, h);
+      center();
+    }
 }
 
 void GRPlotMainWindow::hideTreeWidgetDockSlot()
 {
-  auto w = grplot_widget_->width();
-  auto h = grplot_widget_->height();
-  tree_dock_widget->hide();
-  resizeGRPlotWidget(w, h);
-  center();
+  if (tree_dock_widget->isVisible())
+    {
+      auto w = grplot_widget_->width();
+      auto h = grplot_widget_->height();
+      tree_dock_widget->hide();
+      resizeGRPlotWidget(w, h);
+      center();
+    }
 }
 
 void GRPlotMainWindow::hideTableWidgetDockSlot()
 {
-  auto w = grplot_widget_->width();
-  auto h = grplot_widget_->height();
-  table_dock_widget->hide();
-  resizeGRPlotWidget(w, h);
-  center();
+  if (table_dock_widget->isVisible())
+    {
+      auto w = grplot_widget_->width();
+      auto h = grplot_widget_->height();
+      table_dock_widget->hide();
+      resizeGRPlotWidget(w, h);
+      center();
+    }
 }
 
 void GRPlotMainWindow::hideTextPreviewDockSlot()
 {
-  auto w = grplot_widget_->width();
-  auto h = grplot_widget_->height();
-  text_preview_dock_widget->hide();
-  resizeGRPlotWidget(w, h);
-  center();
+  if (text_preview_dock_widget->isVisible())
+    {
+      auto w = grplot_widget_->width();
+      auto h = grplot_widget_->height();
+      text_preview_dock_widget->hide();
+      resizeGRPlotWidget(w, h);
+      center();
+    }
 }
 
 void GRPlotMainWindow::hideSelectionListDockSlot()
 {
-  auto w = grplot_widget_->width();
-  auto h = grplot_widget_->height();
-  selection_list_dock_widget->hide();
-  resizeGRPlotWidget(w, h);
-  center();
+  if (selection_list_dock_widget->isVisible())
+    {
+      auto w = grplot_widget_->width();
+      auto h = grplot_widget_->height();
+      selection_list_dock_widget->hide();
+      resizeGRPlotWidget(w, h);
+      center();
+    }
 }
 
 void GRPlotMainWindow::hideIconBarDockSlot()
 {
-  auto w = grplot_widget_->width();
-  auto h = grplot_widget_->height();
-  icon_bar_dock_widget->hide();
-  resizeGRPlotWidget(w, h);
-  center();
+  if (icon_bar_dock_widget->isVisible())
+    {
+      auto w = grplot_widget_->width();
+      auto h = grplot_widget_->height();
+      icon_bar_dock_widget->hide();
+      resizeGRPlotWidget(w, h);
+      center();
+    }
 }
 
 void GRPlotMainWindow::hideAddElementDockSlot()
 {
-  auto w = grplot_widget_->width();
-  auto h = grplot_widget_->height();
-  add_element_dock_widget->hide();
-  add_element_dock_widget->setWindowTitle(grplot_widget_->getAddElementWidget()->windowTitle());
-  resizeGRPlotWidget(w, h);
-  center();
+  if (add_element_dock_widget->isVisible())
+    {
+      auto w = grplot_widget_->width();
+      auto h = grplot_widget_->height();
+      add_element_dock_widget->hide();
+      add_element_dock_widget->setWindowTitle(grplot_widget_->getAddElementWidget()->windowTitle());
+      resizeGRPlotWidget(w, h);
+      center();
+    }
 }
 
 void GRPlotMainWindow::closeEditElementDockSlot()

lib/grm/grplot/GRPlotWidget.hxx
@@ -209,6 +209,7 @@ public:
   QAction *getZLimAct();
   QAction *getIconBarAct();
   QAction *getTextColorIndAct();
+  QAction *getDisableGridAct();
   QWidget *getEditElementWidget();
   QWidget *getTreeWidget();
   QWidget *getTableWidget();
@@ -318,6 +319,7 @@ private slots:
   void multipleRadioButtonGroupsListener();
   void colorIndexSlot();
   void possibleElementsMenuSlot();
+  void disableGridSlot();
 
 private:
   struct MouseState
@@ -463,7 +465,7 @@ private:
       *hide_plot_type_sub_menu_act, *show_plot_type_sub_menu_act;
   QAction *x_flip_act, *y_flip_act, *z_flip_act, *theta_flip_act;
   QAction *x_log_act, *y_log_act, *z_log_act, *r_log_act;
-  QAction *use_gr3_act, *polar_with_pan_act, *keep_window_act, *colormap_act, *text_color_ind_act;
+  QAction *use_gr3_act, *polar_with_pan_act, *keep_window_act, *colormap_act, *text_color_ind_act, *disable_grid_act;
   QAction *keep_aspect_ratio_act, *only_square_aspect_ratio_act;
   QAction *vertical_orientation_act, *horizontal_orientation_act;
   QAction *legend_act, *colorbar_act, *left_axis_act, *right_axis_act, *bottom_axis_act, *top_axis_act,

lib/grm/grplot/gredit/EditElementWidget.cxx
@@ -2885,7 +2885,9 @@ bool EditElementWidget::setAttributesDuringAccept(std::shared_ptr<GRM::Element> 
               if (attr_name == "location" && current_selection->localName() == "axis")
                 current_selection->setAttribute("_ignore_next_tick_orientation", true);
               const auto value = static_cast<QComboBox *>(fields[i])->itemText(index).toStdString();
-              grplot_widget->attributeSetForComboBox(attr_type[attr_name], current_selection, value, attr_name);
+              if (!current_selection->hasAttribute(attr_name) ||
+                  value != static_cast<std::string>(current_selection->getAttribute(attr_name)))
+                grplot_widget->attributeSetForComboBox(attr_type[attr_name], current_selection, value, attr_name);
             }
 
           if (attr_name == "colormap")
@@ -2893,17 +2895,24 @@ bool EditElementWidget::setAttributesDuringAccept(std::shared_ptr<GRM::Element> 
               const auto value = static_cast<QComboBox *>(fields[i])->itemText(index).toStdString();
               auto colormap = QPixmap((":/preview_images/colormaps/" + value + ".png").c_str());
               colormap = colormap.scaled(20, 20);
-              grplot_widget->getColormapAct()->setIcon(colormap);
+              if (!current_selection->hasAttribute(attr_name) ||
+                  value != static_cast<std::string>(current_selection->getAttribute(attr_name)))
+                grplot_widget->getColormapAct()->setIcon(colormap);
             }
         }
       else if (typeid(field) == typeid(QCheckBox))
         {
-          current_selection->setAttribute(attr_name, static_cast<QCheckBox *>(fields[i])->isChecked());
+          auto value = static_cast<QCheckBox *>(fields[i])->isChecked();
+          if (!current_selection->hasAttribute(attr_name) ||
+              value != static_cast<int>(current_selection->getAttribute(attr_name)))
+            current_selection->setAttribute(attr_name, value);
         }
       else if (typeid(field) == typeid(QDial))
         {
-          auto val = static_cast<QDial *>(fields[i])->value();
-          current_selection->setAttribute(attr_name, val);
+          auto value = static_cast<QDial *>(fields[i])->value();
+          if (!current_selection->hasAttribute(attr_name) ||
+              value != static_cast<int>(current_selection->getAttribute(attr_name)))
+            current_selection->setAttribute(attr_name, value);
         }
     }
   return highlight_location;

lib/grm/grplot/gredit/IconBarWidget.cxx
@@ -208,6 +208,7 @@ IconBarWidget::IconBarWidget(GRPlotWidget *widget, QWidget *parent) : QWidget(pa
   use_gr3_tool_button->setMinimumWidth(0);
 
   auto use_gr3_act = grplot_widget->getUseGR3Act();
+  use_gr3_act->setToolTip("GR3 or GR variant");
   use_gr3_tool_button->setDefaultAction(use_gr3_act);
   use_gr3_act->setText("");
   name = (grplot_widget->isDarkMode() ? "use_gr3_dark" : "use_gr3");
@@ -223,6 +224,7 @@ IconBarWidget::IconBarWidget(GRPlotWidget *widget, QWidget *parent) : QWidget(pa
   polar_with_pan_tool_button->setMinimumWidth(0);
 
   auto polar_with_pan_act = grplot_widget->getPolarWithPanAct();
+  polar_with_pan_act->setToolTip("Polar With Pan");
   polar_with_pan_tool_button->setDefaultAction(polar_with_pan_act);
   polar_with_pan_act->setText("");
   name = (grplot_widget->isDarkMode() ? "polar_with_pan_dark" : "polar_with_pan");
@@ -238,6 +240,7 @@ IconBarWidget::IconBarWidget(GRPlotWidget *widget, QWidget *parent) : QWidget(pa
   colormap_tool_button->setMinimumWidth(0);
 
   auto colormap_act = grplot_widget->getColormapAct();
+  colormap_act->setToolTip("Colormap");
   colormap_tool_button->setDefaultAction(colormap_act);
 
   text_color_ind_button = new QToolButton(this);
@@ -249,6 +252,7 @@ IconBarWidget::IconBarWidget(GRPlotWidget *widget, QWidget *parent) : QWidget(pa
   text_color_ind_button->setMinimumWidth(0);
 
   auto text_color_ind_act = grplot_widget->getTextColorIndAct();
+  text_color_ind_act->setToolTip("Text Color Index");
   text_color_ind_button->setDefaultAction(text_color_ind_act);
   name = (grplot_widget->isDarkMode() ? "text_color_ind_dark" : "text_color_ind");
   auto text_color_ind = QPixmap((":/icons/" + name + ".png").c_str());
@@ -260,6 +264,22 @@ IconBarWidget::IconBarWidget(GRPlotWidget *widget, QWidget *parent) : QWidget(pa
   orientation_sub_menu->menuAction()->setVisible(false);
   orientation_tool_button->setEnabled(false);
   if (!grplot_widget->getColormapAct()->isVisible()) colormap_tool_button->setVisible(false);
+
+  disable_grid_button = new QToolButton(this);
+  disable_grid_button->setIconSize(QSize(ICON_SIZE, ICON_SIZE));
+  disable_grid_button->setContentsMargins(0, 0, 0, 0);
+  disable_grid_button->setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+  disable_grid_button->setStyleSheet("QToolButton {border:none;} :hover {background: lightgray;}");
+  disable_grid_button->setToolTip("Draw Grid");
+  disable_grid_button->setMinimumWidth(0);
+
+  auto disable_grid_act = grplot_widget->getDisableGridAct();
+  disable_grid_button->setDefaultAction(disable_grid_act);
+  disable_grid_act->setText("");
+  disable_grid_act->setToolTip("Draw Grid");
+  name = (grplot_widget->isDarkMode() ? "disable_grid_dark" : "disable_grid");
+  auto disable_grid = QPixmap((":/icons/" + name + ".png").c_str());
+  disable_grid_act->setIcon(disable_grid);
 
   connect(grplot_widget->getHideAlgoMenuAct(), &QAction::triggered, this, &IconBarWidget::hideAlgoMenu);
   connect(grplot_widget->getShowAlgoMenuAct(), &QAction::triggered, this, &IconBarWidget::showAlgoMenu);
@@ -296,6 +316,7 @@ IconBarWidget::IconBarWidget(GRPlotWidget *widget, QWidget *parent) : QWidget(pa
   h_box_layout->addWidget(polar_with_pan_tool_button);
   h_box_layout->addWidget(colormap_tool_button);
   h_box_layout->addWidget(text_color_ind_button);
+  h_box_layout->addWidget(disable_grid_button);
 
   h_box_layout->setContentsMargins(0, 0, 0, 0);
   h_box_layout->setAlignment(Qt::AlignLeft);

lib/grm/grplot/gredit/IconBarWidget.hxx
@@ -17,7 +17,7 @@ private:
   GRPlotWidget *grplot_widget;
   QToolButton *type_tool_button, *algo_tool_button, *log_tool_button, *flip_tool_button, *orientation_tool_button,
       *aspect_ratio_tool_button, *location_tool_button, *lim_tool_button, *use_gr3_tool_button,
-      *polar_with_pan_tool_button, *colormap_tool_button, *text_color_ind_button;
+      *polar_with_pan_tool_button, *colormap_tool_button, *text_color_ind_button, *disable_grid_button;
   QMenu *type_sub_menu, *marginal_sub_menu, *algo_sub_menu, *log_sub_menu, *flip_sub_menu, *orientation_sub_menu,
       *aspect_ratio_sub_menu, *location_sub_menu, *lim_sub_menu;
   QHBoxLayout *h_box_layout;

commit de6027a14553415e3c13dd02f6de9d9c9cbc734f

Fix segfault in grm_export when no figure is active
grm_export dereferenced the result of
querySelectors("figure[active=\"1\"]") without checking for nullptr.
When grm_export is called before anything has been plotted (e.g. the
grm_merge -> grm_export flow used by language bindings), no active
figure exists in the graphics tree yet, so active_figure is nullptr and
the following querySelectors call crashes with SIGSEGV (fault address
0x18, inside GRM::Node::nodeType).

Guard the plot lookups with a nullptr check, matching the existing
active_plot null checks below. Regression introduced in b7520a3c
(first released in v0.73.23).

lib/grm/src/grm/plot.cxx
@@ -6975,8 +6975,12 @@ int grm_process_tree(void)
 int grm_export(const char *file_path, int export_xml)
 {
   auto active_figure = global_root->querySelectors("figure[active=\"1\"]");
-  auto active_plot = active_figure->querySelectors("plot[_active=\"1\"]");
-  auto active_plot_through_update = active_figure->querySelectors("plot[_active_through_update=\"1\"]");
+  std::shared_ptr<GRM::Element> active_plot, active_plot_through_update;
+  if (active_figure != nullptr)
+    {
+      active_plot = active_figure->querySelectors("plot[_active=\"1\"]");
+      active_plot_through_update = active_figure->querySelectors("plot[_active_through_update=\"1\"]");
+    }
 
   if (active_plot != nullptr) active_plot->setAttribute("_active", 0);
   if (active_plot_through_update != nullptr) active_plot_through_update->setAttribute("_active_through_update", 0);